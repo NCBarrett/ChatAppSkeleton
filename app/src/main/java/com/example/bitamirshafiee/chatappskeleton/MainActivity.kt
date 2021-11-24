c9ba147807980984986540bd8f8936d2edf7902b
Neill Barrett <barrettneill@gmail.com>
2021-11-24 01:54:25 -0800

    Getting started with Firebase

---
+++ app/src/main/java/com/example/bitamirshafiee/chatappskeleton/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bitamirshafiee.chatappskeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+//    private var firebaseAdapter: FirebaseRecyclerAdapter? = null  //problem line
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
