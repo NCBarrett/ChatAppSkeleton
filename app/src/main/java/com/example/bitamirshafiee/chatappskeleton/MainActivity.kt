820495959bb50cb72f21a9947e48fa6c0ca6ffec
Neill Barrett <barrettneill@gmail.com>
2021-12-03 23:37:35 -0800

    Getting started with Firebase. Dependency issue fixed. Building out activity code 12/3/21

--- app/src/main/java/com/example/bitamirshafiee/chatappskeleton/MainActivity.kt
+++ app/src/main/java/com/example/bitamirshafiee/chatappskeleton/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.example.bitamirshafiee.chatappskeleton
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "MainActivity"
+        const val ANONYMOUS = "anonymous"
+    }
 
+    private var userName : String? = null
+    private var userPhotoUrl : String? = null
+
+    private var fireBaseAuth : FirebaseAuth? = null
+    private var fireBaseUser : FirebaseUser? = null
+
+//    private var googleApiClient : GoogleApiClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        userName = ANONYMOUS
+
+        fireBaseAuth = FirebaseAuth.getInstance()
+        fireBaseUser = fireBaseAuth!!.currentUser
+
+        if (fireBaseUser == null) {
+            startActivity(Intent (this@MainActivity, SignInActivity::class.java))
+            finish()
+        } else {
+            userName = fireBaseUser!!.displayName
+
+            if (fireBaseUser!!.photoUrl != null) {
+                userPhotoUrl = fireBaseUser!!.photoUrl!!.toString()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bitamirshafiee/chatappskeleton/SignInActivity.kt
+++ app/src/main/java/com/example/bitamirshafiee/chatappskeleton/SignInActivity.kt
@@ -2,10 +2,28 @@ package com.example.bitamirshafiee.chatappskeleton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+    private var googleSignInClient : GoogleSignInClient? = null
+    private var fireBaseAuth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        fireBaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this@SignInActivity, gso)
+        
     }
 }
